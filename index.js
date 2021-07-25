eb271385c8a848eac689193290d0a1591f92b92a
soloveyy <33604729+soloveyy@users.noreply.github.com>
2021-07-25 14:35:04 +0200

    Init file index.js create function getMedia and add database id to the file

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const dotenv = require('todenv').config()
+const dotenv = require('dotenv')
+const {Client} = require('@notionhq/client')
+
+
+dotenv.config()
+const notion = new Client({
+    auth: process.env.NOTION_KEY
+})
+
+const databaseId = process.env.NOTION_DATABASE_ID
+
+// const listDatabases = async () => {
+//     const res = await notion.databases.list()
+//     console.log(res.title[0]);
+// }
+
+// listDatabases()
+
+const getMedia = async () => {
+    const payload = {
+        path: `databases/${databaseId}/query`,
+        method: 'POST',
+
+    }
+    const {results} = await notion.request(payload)
+
+    const eachData = results.map(page=>console.log(page.properties.Name.title[0].text.content))
+}
+
+getMedia()
