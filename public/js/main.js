d81209e7a3d451cdda501564648f318f42498b9c
soloveyy <33604729+soloveyy@users.noreply.github.com>
2021-08-21 19:28:19 +0200

    Add logic to main.js

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+const loading = document.getElementById('loading')
+const content = document.getElementById('content')
+
+let loadingState = false
+
+const getMediaFromBack = async() => {
+    loadingState = true
+    const res = await fetch('http://localhost:5000/media')
+    const data = await res.json()
+    loadingState = false
+    return data
+}
+
+const showMediaContent = async () => {
+    const media = await getMediaFromBack()
+    
+    media.forEach(el => {
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <h1>${el.title}</h1>
+        <p>${el.type}</p>
+        <p>${el.date}</p>
+        <a>${el.url}</a>
+        `
+        content.appendChild(div)   
+    });
+}
+
+showMediaContent()
